9b46a2065dc62003532dd9fb3056b8b408a62ff9
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2022-02-02 16:47:02 -0300

    ajustes no layout

--- src/assets/data/services.js
+++ src/assets/data/services.js
@@ -11,35 +11,35 @@ const projects = [
     name: 'Desentupimento de vaso sanitário',
     desc:
       'Realizando desentupimento de vasos sanitários de comércios e residências.',
-    img: UTrackerImg,
+    img: "https://cdn.icon-icons.com/icons2/1993/PNG/512/frame_gallery_image_images_photo_picture_pictures_icon_123209.png",
   },
   {
     id: uuidv4(),
     name: 'Desentupimento de ralo',
     desc:
       'Realizando desentupimento de ralos de comércios e residências.',
-    img: GreenCtgImg,
+    img: "https://cdn.icon-icons.com/icons2/1993/PNG/512/frame_gallery_image_images_photo_picture_pictures_icon_123209.png",
   },
   {
     id: uuidv4(),
     name: 'Desentupimento de pia',
     desc:
       'Realizando desentupimento de pia de comércios e residências.',
-    img: CoinTrackerImg,
+    img: "https://cdn.icon-icons.com/icons2/1993/PNG/512/frame_gallery_image_images_photo_picture_pictures_icon_123209.png",
   },
   {
     id: uuidv4(),
     name: "Desentupimento de esgotos",
     desc:
       'Realizando desentupimento de de esgotos de comércios e residências.',
-    img: CavinImg,
+    img: "https://cdn.icon-icons.com/icons2/1993/PNG/512/frame_gallery_image_images_photo_picture_pictures_icon_123209.png",
   },
   {
     id: uuidv4(),
     name: 'Desentupimento de cano',
     desc:
       'Realizando desentupimento de cano de comércios e residências.',
-    img: ProjectImg,
+    img: "https://cdn.icon-icons.com/icons2/1993/PNG/512/frame_gallery_image_images_photo_picture_pictures_icon_123209.png",
   },
 ];
 
